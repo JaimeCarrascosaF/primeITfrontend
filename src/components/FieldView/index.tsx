c20338e5f8facc29b6dd7e4fb5397f60f6cf9f62
Jaime Carrascosa <inf.jaime1@gmail.com>
2024-01-02 19:21:00 +0100

    Some styles

--- src/components/FieldView/index.tsx
+++ src/components/FieldView/index.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { Input, Modal } from "antd";
+import { Input, Modal, Typography } from "antd";
 import TextArea from "antd/es/input/TextArea";
 import { changeFieldData, handleCancel, handleOk } from "./actions";
 import { valueType } from "antd/es/statistic/utils";
+import "./index.css";
 
 const ModalComponent = ({
   open,
@@ -21,18 +22,34 @@ const ModalComponent = ({
   setTitle: React.Dispatch<React.SetStateAction<valueType>>;
   setDetails: React.Dispatch<React.SetStateAction<valueType>>;
 }) => {
-  const [titleStatus, setTitleStatus] = useState<"" | "warning" | "error" | undefined>("");
-  const [detailsStatus, setDetailsStatus] = useState<"" | "warning" | "error" | undefined>("");
+  const [titleStatus, setTitleStatus] = useState<
+    "" | "warning" | "error" | undefined
+  >("");
+  const [detailsStatus, setDetailsStatus] = useState<
+    "" | "warning" | "error" | undefined
+  >("");
 
   return (
     <>
       <Modal
-        title="Todo item modal"
         open={open}
-        onOk={() => handleOk({ setOpen, titleStatus, detailsStatus, item: {title, details, id} })}
-        onCancel={() => handleCancel({ setOpen, setTitleStatus, setDetailsStatus })}
+        onOk={() =>
+          handleOk({
+            setOpen,
+            titleStatus,
+            detailsStatus,
+            item: { title, details, id },
+          })
+        }
+        onCancel={() =>
+          handleCancel({ setOpen, setTitleStatus, setDetailsStatus })
+        }
       >
+        <Typography.Title level={3}>
+        Title
+        </Typography.Title>
         <Input
+          className="titleInput"
           placeholder="Title"
           status={titleStatus}
           onChange={(chEl) =>
@@ -44,7 +61,11 @@ const ModalComponent = ({
           }
           value={title}
         />
+        <Typography.Title level={3}>
+        Details
+        </Typography.Title>
         <TextArea
+          className="detailsInput"
           rows={2}
           status={detailsStatus}
           onChange={(chEl) =>
